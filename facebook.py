5c4116102b2d437d251b5d6e3e8d82f06638a446
layan21-meet <52973538+layan21-meet@users.noreply.github.com>
2019-12-18 12:10:41 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -5,16 +5,16 @@ class User(object):
 		self.name = name
 		self.email = email
 		self.password = password
-		self.friends_list = friends_list
-		self.posts = posts
-   	def add_friend(self,email):
+		self.friends_list = []
+		self.posts = []
+	def add_friend(self,email):
 		self.friends_list.append(email)
 		print(self.name+" has added "+email+" as a friend")
-    def remove_friend(self,email):	
+	def remove_friend(self,email):
 		self.friends_list.remove(email)
- 		print(self.name+" has removed "+email+" as a friend")
-    def post(self,text):
-		self.posts.append(text)
+		print(self.name+" has removed "+email+" from friends")
+	def create_post(self,text):
+		self.posts.append(text )
 		print(self.name+" has posted "+ text)
 		post1 = Post("post", self.name)
 	def get_userinfo(self):
@@ -23,28 +23,37 @@ class User(object):
 		print("Password:"+self.password)
 		print("Friends:"+str(self.friends_list))
 		print("Posts:"+str(self.posts))
-
 class Post(object):
 	def __init__(self,name,caption,comments=[]):
 		self.name = name
 		self.caption = caption
 		self.comments = []
-	def add_caption(self,caption_text):
-		self.caption_text = caption_text
-		self.caption.append(caption_text)
-		print(self.name + " has added the caption " + caption_text)
-	def add_comment(self, comment_text):
+
+	def add_caption(self,text):
+		self.caption = text 
+		print(self.name + " has added a caption " + self.caption)
+	def add_comment(self,caption_text):
 		self.comment_text = comment_text
 		self.comment.append(comment_text)
 		print(self.name + " has added a comment " + comment_text)
 	def like_post(self):
 		print(self.name + " has liked your post!")
-user1 = User("Layan","layann.hamdan@gmail.com","LaYan2019",0,0)
-user2 = User("Hiba","Hiba22@gmail.com","Hibaaa9102",0,0)
-user1.add_friend(user2.email)
-user1.post("Hi")
-user1.get_userinfo()
-user1.remove_friend(user2.email)
-user1.post("a new post")
 
+class Comment(Post):
+	pass
+login = input("login or sign up?")
+if login =="sign up":
+	name1 = input("Please enter an email")
+	email = input("Birthday Date")
+
+
+user1 = User("Layan","layann.hamdan@gmail.com","LaYan2019",0,0)
+user2 = User("Celine","Celine@gmail.com","Hibaaa9102",0,0)
+user1.add_friend("Hiba22@gmail.com")
+user2.create_post("Happyyy place")
+user1.get_userinfo()
+user2.get_userinfo()
+user1.remove_friend("Hiba22@gmail.com")
+post1 = Post("layan", "Pizzzzaa Love","comment")
+post1.add_caption("hhiiiiiiiiiiiiiiiiiii")
 
