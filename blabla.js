2d23fac402410a23c0ab541ad15b5a9e3796dca2
layan21-meet <52973538+layan21-meet@users.noreply.github.com>
2020-01-08 12:22:07 -0500

    Add files via upload

---
+++ blabla.js
@@ -0,0 +1,10 @@
+console.log("Hello, world");
+var birthyear= parseInt(prompt("What you were you born in?"));
+var luckynumber=parseInt(prompt("What is your luckynumber?"));
+var favFruit = parseInt(prompt("What is your favFruit?"));
+var favsubject = parseInt(prompt("What is your favsubject?"));
+alert(birthyear/luckynumber);
+alert(birthyear+luckynumber);
+alert(birthYear + favSubject);
+alert(favFruit + birthYear + luckyNumber);
+alert( birthYear + luckyNumber + favSubject)
